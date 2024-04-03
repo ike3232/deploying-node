7d6793252414ac374693e69e1b208a32b1a6bcb0
ike3232 <iketonijon@gmail.com>
2024-04-03 13:15:25 +0100

    first commit

---
+++ build/precache-manifest.60ae0584ea5db39cbad60637389b3417.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3300a5710e13e8c9785ad7e4e10c2336",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8e46e5560fcf9eee6082",
+    "url": "/static/css/2.7f1403ef.chunk.css"
+  },
+  {
+    "revision": "c8488ff0af59aa339a44",
+    "url": "/static/css/main.4289a7c4.chunk.css"
+  },
+  {
+    "revision": "8e46e5560fcf9eee6082",
+    "url": "/static/js/2.335d95f6.chunk.js"
+  },
+  {
+    "revision": "c8488ff0af59aa339a44",
+    "url": "/static/js/main.114dd2f0.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "9f7e506d7455da27ce24e474c84a8e0a",
+    "url": "/static/media/logo.9f7e506d.svg"
+  }
+]);
